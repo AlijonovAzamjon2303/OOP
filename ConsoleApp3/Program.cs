2458d4be67486e0fc848957daf833eac93f62f1a
A'zamjon <alijonovrobo@gmail.com>
2024-02-28 19:33:42 +0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,101 @@
+﻿using ConsoleApp3.Models;
+using ConsoleApp3.Services;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PhoneBookServices phoneBookServices = new PhoneBookServices(10);
+        string input = "";
+        do
+        {
+            phoneBookServices.PrintMenu();
+
+            input = Console.ReadLine(); 
+            switch(input)
+            {
+                case "1":
+                    {
+                        Console.Clear();    
+                        phoneBookServices.ReadAllPhoneBooks();
+                        if(phoneBookServices.Count == 0)
+                        {
+                            Console.WriteLine("Bizda hali ro'yxat shakllanmagan!");
+                        }
+                    }
+                    break;
+                
+                case "2":
+                    {
+                        Console.Clear();
+                        PhoneBook phoneBook = new PhoneBook();
+                        Console.Write("Ism va familiya : "); phoneBook.Name = Console.ReadLine();
+                        Console.Write("Telefon raqam : "); phoneBook.PhoneNumber = Console.ReadLine();
+
+                        phoneBookServices.AddPhoneBook(phoneBook);
+                    }
+                    break;
+                case "3":
+                    {
+                        Console.Clear();
+                        Console.Write("Id = ");
+                        int n = int.Parse(Console.ReadLine());
+                        
+                        if(phoneBookServices.Contains(n))
+                        {
+                            phoneBookServices.ReadById(n);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{n} id li mijoz topilmadi!");
+                        }
+                    }
+                    break;
+                case "4":
+                    {
+                        Console.Clear();
+                        Console.Write("O'chirmoqchi bo'lgan mijoz Id = ");
+                        int n = int.Parse(Console.ReadLine());
+
+                        if(phoneBookServices.Contains(n))
+                        {
+                            phoneBookServices.DeletedById(n);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{n} id li mijoz topilmadi!");
+                        }
+                    }
+                    break;
+                case "5":
+                    {
+                        PhoneBook newPhoneBook = new PhoneBook();
+                        Console.Clear();
+                        Console.Write("O'zgartirmoqchi bo'lgan mijoz Id = ");
+                        int id = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Avvalgi ma'lumotlari : ");
+                        phoneBookServices.ReadById(id);
+                        
+                        if(phoneBookServices.Contains(id))
+                        {
+                            Console.Write("\nYangi ismi : ");
+                            newPhoneBook.Name = Console.ReadLine();
+                            Console.Write("\nYangi nomeri : ");
+                            newPhoneBook.PhoneNumber = Console.ReadLine();
+                            newPhoneBook.Id = id;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{id} idli mijoz topilmadi!");
+                        }
+
+                        phoneBookServices.UpdateById(id, newPhoneBook);
+                    }
+                    break;
+            }
+
+        } while (input != "0");
+    }
+}
---
+++ ConsoleApp3/Services/PhoneBookServices.cs
@@ -0,0 +1,111 @@
+﻿using ConsoleApp3.Models;
+
+namespace ConsoleApp3.Services
+{
+    internal class PhoneBookServices
+    {
+        PhoneBook[] phoneBooks;
+
+        int count = 0;
+        public int Count 
+        {
+           get => count;
+        }
+        private int id = 1;
+        public PhoneBookServices(int n)
+        {
+            phoneBooks = new PhoneBook[n];  
+        }
+
+        public void AddPhoneBook(PhoneBook phoneBook)
+        {
+            count++;
+            phoneBook.Id = id++;
+            for(int iteration = 0; iteration < phoneBooks.Length; iteration++)
+            {
+                if (phoneBooks[iteration] == null) 
+                {
+                    phoneBooks[iteration] = phoneBook;
+                    break;
+                }
+            }
+        }
+
+        public void ReadAllPhoneBooks()
+        {
+            for(int i = 0; i < phoneBooks.Length; i++)
+            {
+                if (phoneBooks[i] != null)
+                {
+                    Console.WriteLine($"{phoneBooks[i].Name} : {phoneBooks[i].PhoneNumber}");    
+                }
+            }
+        }
+
+        public void ReadById(int id)
+        {
+            Console.Clear();
+            for(int i = 0; i < phoneBooks.Length; i++)
+            {
+                if (phoneBooks[i] != null && phoneBooks[i].Id == id)
+                {  
+                    Console.WriteLine($"{phoneBooks[i].Name} : {phoneBooks[i].PhoneNumber}");
+                }
+            }
+        }
+
+        public void DeletedById(int id)
+        {
+            bool isDeleted = false;
+            for(int i = 0; i < phoneBooks.Length; i++) 
+            {
+                if (phoneBooks[i] != null && phoneBooks[i].Id == id)
+                {
+                    phoneBooks[i] = null;
+                    isDeleted = true;   
+                    count--;
+                }
+            }
+
+            if(isDeleted)
+            {
+                Console.WriteLine($"{id} idli mijoz o'chirildi!");
+            }
+        }
+
+        public void UpdateById(int id, PhoneBook newPhoneBook)
+        {
+            for(int i = 0; i < phoneBooks.Length; i++)
+            {
+                if (phoneBooks[i] != null && phoneBooks[i].Id == id)
+                {
+                    phoneBooks[i] = newPhoneBook;
+                }
+            }
+        }
+
+        public void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. ReadAllPhoneBooks");
+            Console.WriteLine("2. AddPhoneBook");
+            Console.WriteLine("3. ReadById");
+            Console.WriteLine("4. DeleteById");
+            Console.WriteLine("5. UpdateById");
+            Console.WriteLine("0. Dasturni tugatish");
+        }
+
+        public bool Contains(int id)
+        {
+            for(int i = 0;i < phoneBooks.Length;i++)
+            {
+                if (phoneBooks[i] != null && phoneBooks[i].Id == id)
+                {
+                    return true;
+                }
+            }
+
+            return false;   
+        }
+    }
+}
